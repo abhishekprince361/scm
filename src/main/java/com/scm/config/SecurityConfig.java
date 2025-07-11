718f47a2d4949bdc0895bfb1783902eb9d6f7b9e
Abhishek <abhishekprince361@gmail.com>
2025-07-11 21:45:00 +0530

    adding contact

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,28 +22,29 @@ public class SecurityConfig {
 
     // @Bean
     // private UserDetailsService userDetailsService(){
-    //     // this bean use in memory it don't use the database
-    //     UserDetails user1 = User.withDefaultPasswordEncoder().withUsername("admin123").password("admin123").roles("ADMIN", "USER").build();
+    // // this bean use in memory it don't use the database
+    // UserDetails user1 =
+    // User.withDefaultPasswordEncoder().withUsername("admin123").password("admin123").roles("ADMIN",
+    // "USER").build();
 
-    //     // we can insert multiple user also
+    // // we can insert multiple user also
 
-    //     var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1);
-    //     return inMemoryUserDetailsManager;
+    // var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1);
+    // return inMemoryUserDetailsManager;
 
     // }
     @Bean
     @SuppressWarnings("deprecation")
-    public DaoAuthenticationProvider authenticationProvider(){
+    public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-
 
         return daoAuthenticationProvider;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.authorizeHttpRequests(authorize -> {
             // authorize.requestMatchers("/home", "/register").permitAll();
@@ -55,10 +55,10 @@ public class SecurityConfig {
         httpSecurity.formLogin(formLogin -> {
             // override the spring login
             formLogin.loginPage("/login");
-            // processing and submit on this url 
+            // processing and submit on this url
             formLogin.loginProcessingUrl("/authenticate");
-            // after successful login 
-            formLogin.successForwardUrl("/user/dashboard");
+            // after successful login
+            formLogin.successForwardUrl("/user/profile");
             // if got any error
             // formLogin.failureForwardUrl("/login?error=true");
             // customize username field by default it is (username)
@@ -66,52 +66,57 @@ public class SecurityConfig {
             formLogin.passwordParameter("password");
 
             /*
-            formLogin.successHandler(new AuthenticationSuccessHandler() {
-
-                @Override
-                public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                        Authentication authentication) throws IOException, ServletException {
-                    
-                    throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
-                }
-                
-            });
-
-            formLogin.successHandler(new AuthenticationSuccessHandler() {
-
-                @Override
-                public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                        Authentication authentication) throws IOException, ServletException {
-
-                   throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
-                }
-            
-            });
-            */
+             * formLogin.successHandler(new AuthenticationSuccessHandler() {
+             * 
+             * @Override
+             * public void onAuthenticationSuccess(HttpServletRequest request,
+             * HttpServletResponse response,
+             * Authentication authentication) throws IOException, ServletException {
+             * 
+             * throw new
+             * UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'"
+             * );
+             * }
+             * 
+             * });
+             * 
+             * formLogin.successHandler(new AuthenticationSuccessHandler() {
+             * 
+             * @Override
+             * public void onAuthenticationSuccess(HttpServletRequest request,
+             * HttpServletResponse response,
+             * Authentication authentication) throws IOException, ServletException {
+             * 
+             * throw new
+             * UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'"
+             * );
+             * }
+             * 
+             * });
+             */
         });
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
-        // by default it is get method but logout required post method because it is secure with csrf so we have to desable csrf
-        // csrf is activatet by default because spring is using EnableWebSecurity
-        httpSecurity.logout(logoutForm-> {
-            logoutForm.logoutUrl("/logout");
-            logoutForm.logoutSuccessUrl("/login?logout?true");
-        });
-
-        //oauth configuration
-        httpSecurity.oauth2Login(oauth->{
+        // oauth configuration
+        httpSecurity.oauth2Login(oauth -> {
             oauth.loginPage("/login");
             oauth.successHandler(oAuthAuthenicationsuccessHandler);
         });
 
+        // by default it is get method but logout required post method because it is
+        // secure with csrf so we have to desable csrf
+        // csrf is activatet by default because spring is using EnableWebSecurity
+        httpSecurity.logout(logoutForm -> {
+            logoutForm.logoutUrl("/do-logout");
+            logoutForm.logoutSuccessUrl("/login?logout?true");
+        });
 
         return httpSecurity.build();
     }
 
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
