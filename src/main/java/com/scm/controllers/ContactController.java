9fedeaa00dcf1c7c36614ada1bde1c4cf0c35a80
Abhishek <abhishekprince361@gmail.com>
2025-07-12 21:31:13 +0530

    validate contact form and save to database

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,15 +1,72 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.scm.entities.Contact;
+import com.scm.entities.User;
+import com.scm.forms.ContactForm;
+import com.scm.helpers.Helper;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
+import com.scm.services.ContactService;
+import com.scm.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
 
-    //add Contact Page: handler
+    @Autowired
+    private ContactService contactService;
+
+    @Autowired
+    private UserService userService;
+
+    // add Contact Page: handler
     @RequestMapping("/add")
-    public String addContactView(){
+    public String addContactView(Model model) {
+
+        ContactForm contactForm = new ContactForm();
+        model.addAttribute(contactForm);
         return "user/add_contact";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult result,
+            Authentication authentication, HttpSession session) {
+
+        if (result.hasErrors()){
+            session.setAttribute("message", Message.builder().content("Please Correct The Errors.").type(MessageType.red).build());
+            return "user/add_contact";
+        }
+
+        String username = Helper.getEmailOfLoggedInUser(authentication);
+        User user = userService.getUserByEmail(username);
+
+        Contact contact = new Contact();
+        contact.setUser(user);
+        contact.setName(contactForm.getName());
+        contact.setEmail(contactForm.getEmail());
+        contact.setPhoneNumber(contactForm.getPhoneNumber());
+        contact.setAddress(contactForm.getAddress());
+        contact.setDescription(contactForm.getDescription());
+        contact.setFavorite(contactForm.isFavorite());
+        contact.setLinkedInLink(contactForm.getLinkedInLink());
+        contact.setWebsiteLink(contactForm.getWebsiteLink());
+        contact.setPicture(null);
+
+        contactService.save(contact);
+
+        session.setAttribute("message",Message.builder().content("You have successfully added a new contact").type(MessageType.green).build());
+        return "redirect:/user/contacts/add";
     }
 }
---
+++ src/main/java/com/scm/forms/ContactForm.java
@@ -0,0 +1,40 @@
+package com.scm.forms;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ContactForm {
+
+    @NotBlank(message = "Name is Required")
+    private String name;
+
+    @NotBlank(message = "Email is Required")
+    @Email(message = "Invalid Email Address [example@gmail.com]")
+    private String email;
+
+    @NotBlank(message = "Phone Number is Required")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Invalid Phone Number")
+    private String phoneNumber;
+
+    @NotBlank(message = "Address is Required")
+    private String address;
+    private String description;
+    private boolean favorite;
+    private String websiteLink;
+    private String linkedInLink;
+    private MultipartFile profileImage;
+
+}
